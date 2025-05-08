c2ca16b79cde134ee195b7a9e2fd747bba4b7373
GianMen91 <giancarlo.mennillo@gmail.com>
2025-05-08 18:51:41 +0200

    add todo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,11 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tinder_for_movies/main.dart';
 
+//TODO implements tests
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
