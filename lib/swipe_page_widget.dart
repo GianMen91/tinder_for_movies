ce87b2e0655f2ae1224f49e6e8406d72b31e9d4f
GianMen91 <giancarlo.mennillo@gmail.com>
2025-05-08 12:27:12 +0200

    merge classes

--- lib/swipe_page_widget.dart
+++ lib/swipe_page_widget.dart
@@ -1,12 +1,107 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tinder_for_movies/swipeable_stack.dart'; // Original SwipeableStack
 import 'package:tinder_for_movies/swipeable_stack_controller.dart'; // Separate controller file
 
 import 'movie_details_widget.dart';
 import 'movies_record.dart';
+
+
+class SwipeableStack extends StatefulWidget {
+  const SwipeableStack({
+    super.key,
+    required this.itemBuilder,
+    required this.itemCount,
+    this.onSwipeFn,
+    this.onLeftSwipe,
+    this.onRightSwipe,
+    this.onUpSwipe,
+    this.onDownSwipe,
+    this.loop = false,
+    this.cardDisplayCount = 3,
+    this.scale = 0.9,
+    this.controller, // Added controller parameter
+  });
+
+  final Widget Function(BuildContext, int) itemBuilder;
+  final int itemCount;
+
+  final Function(int)? onSwipeFn;
+  final Function(int)? onLeftSwipe;
+  final Function(int)? onRightSwipe;
+  final Function(int)? onUpSwipe;
+  final Function(int)? onDownSwipe;
+  final bool loop;
+  final int cardDisplayCount;
+  final double scale;
+  final SwipeableStackController? controller; // Controller for programmatic swipes
+
+  @override
+  SwipeableStackState createState() => SwipeableStackState();
+}
+
+class SwipeableStackState extends State<SwipeableStack> {
+  // Create a CardSwiperController
+  late CardSwiperController _cardSwiperController;
+
+  @override
+  void initState() {
+    super.initState();
+    _cardSwiperController = CardSwiperController();
+
+    // Connect the controller if provided
+    if (widget.controller != null) {
+      widget.controller!.setCardSwiperController(_cardSwiperController);
+    }
+  }
+
+  @override
+  void dispose() {
+    _cardSwiperController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CardSwiper(
+      controller: _cardSwiperController, // Pass the controller to CardSwiper
+      cardsCount: widget.itemCount,
+      cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+          widget.itemBuilder(context, index),
+      onSwipe: (int previousIndex, int? currentIndex,
+          CardSwiperDirection direction) {
+        widget.onSwipeFn?.call(previousIndex);
+
+        switch (direction) {
+          case CardSwiperDirection.left:
+            widget.onLeftSwipe?.call(previousIndex);
+            break;
+          case CardSwiperDirection.right:
+            widget.onRightSwipe?.call(previousIndex);
+            break;
+          case CardSwiperDirection.top:
+            widget.onUpSwipe?.call(previousIndex);
+            break;
+          case CardSwiperDirection.bottom:
+            widget.onDownSwipe?.call(previousIndex);
+            break;
+          default:
+            break;
+        }
+
+        return true;
+      },
+      isLoop: widget.loop,
+      numberOfCardsDisplayed: widget.cardDisplayCount,
+      scale: widget.scale,
+      backCardOffset: const Offset(0, 0),
+    );
+  }
+}
+
 
 class SwipePageWidget extends StatefulWidget {
   const SwipePageWidget({super.key});
@@ -281,4 +376,64 @@ DocumentReference? get currentUserReference {
   return user != null
       ? FirebaseFirestore.instance.collection('users').doc(user.uid)
       : null;
+}
+
+class SwipeableStackController {
+  CardSwiperController? _cardSwiperController;
+
+  // Connect to the CardSwiperController
+  void setCardSwiperController(CardSwiperController controller) {
+    _cardSwiperController = controller;
+  }
+
+  // Methods to trigger swipes programmatically
+  void swipeLeft() {
+    if (_cardSwiperController != null) {
+      try {
+        _cardSwiperController!.swipeLeft();
+      } catch (e) {
+        print("Error swiping left: $e");
+      }
+    }
+  }
+
+
+
+  void swipeRight() {
+    if (_cardSwiperController != null) {
+      try {
+        _cardSwiperController!.swipeRight();
+      } catch (e) {
+        print("Error swiping right: $e");
+      }
+    }
+  }
+
+  void swipeUp() {
+    if (_cardSwiperController != null) {
+      try {
+        _cardSwiperController!.swipeTop();
+      } catch (e) {
+        print("Error swiping up: $e");
+      }
+    }
+  }
+
+  void swipeDown() {
+    if (_cardSwiperController != null) {
+      try {
+        _cardSwiperController!.swipeBottom();
+      } catch (e) {
+        print("Error swiping down: $e");
+      }
+    }
+  }
+
+  // Method to check if controller is ready
+  bool get isReady => _cardSwiperController != null;
+
+  // Clean up
+  void dispose() {
+    _cardSwiperController = null;
+  }
 }
